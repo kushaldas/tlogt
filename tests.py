6cde80c82aa3cfc6b59d3c67b7c8964a2bb4830d
Kushal Das <kushal@fedoraproject.org>
2012-05-19 22:34:43 +0530

    tests.py: Fixed the test for sudo
    Description
    ============
    Parses sudo output properly. Currently too sleepy to code.

--- tests.py
+++ tests.py
@@ -1,15 +1,16 @@
 import unittest
 from pprint import pprint
 
-class SecureGetLogTest(unittest.TestCase):
+class SecureSudoTest(unittest.TestCase):
     """
     To test secure log details
     """
     def runTest(self):
         from tlogtparser import Secure
         data = Secure.get_log('testdata/secure')
-        for datum in data:
-            pprint(datum)
+        assert data[97]['status'] == 'warning'
+        assert data[99]['status'] == 'error'
+        
         
 if __name__ == '__main__':
     unittest.main()
